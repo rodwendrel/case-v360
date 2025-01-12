27743900c52f016375615012e2e8c30e9956a2e3
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2025-01-12 19:57:56 -0300

    fix: botão de remover tarefas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
   root "lists#index"
 
   resources :lists do
-    resources :tasks, only: [:create, :destroy] 
+    resources :tasks, only: [:create, :destroy]
   end
 end
