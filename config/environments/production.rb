395ac40a245427cf28d74b797799db2b85bd66c5
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2025-01-14 00:40:01 -0300

    fixing deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,10 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.action_cable.allowed_request_origins = []
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.url = nil
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
