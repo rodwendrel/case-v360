23f980e7b02430104825c56b3b8d409d5d85036d
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2025-01-14 00:04:22 -0300

    fix deploy key

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,7 @@ module CaseV360
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
-    # Set this to false if you do not require a master key for credentials.
+    # Do not require a master key for credentials.
     config.require_master_key = false
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
   end
 end
