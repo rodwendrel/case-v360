228cf3b47fa7731aa77a87fa780bc43de58c09fe
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2025-01-13 23:58:29 -0300

    fix deploy key

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-config.require_master_key = false
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,17 +11,11 @@ module CaseV360
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    # Set this to false if you do not require a master key for credentials.
+    config.require_master_key = false
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
